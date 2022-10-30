701a10f565fb5cbabaac493eab00119e5193d5af
Nopxz <naufalps42@gmail.com>
2022-10-30 12:20:00 +0700

    Membuat Aplikasi Kasir Penjualan

---
+++ src/test/java/takehome1/takehomewebservice/TakehomewebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package takehome1.takehomewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakehomewebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
